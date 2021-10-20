09e6f6376b0b6049318994ef9024f96edf2e8f2f
Jérôme Coupé <jercoupe@gmail.com>
2021-10-20 10:24:55 +0200

    switch to identity validate

--- functions/identity-validate.js
+++ functions/identity-validate.js
@@ -1,7 +1,13 @@
 exports.handler = function (event, context, callback) {
+  const data = JSON.parse(event.body);
+  const { user } = data;
+
   const responseBody = {
     app_metadata: {
       roles: ["admin"],
+    },
+    user_metadata: {
+      ...user.user_metadata,
     },
   };
   callback(null, {
